89675932aac8942295d75c500b1c6ffa94be0770
Prateep Panyanontakarn <139861072+prateeppyntk@users.noreply.github.com>
2024-05-01 03:27:09 +0000

    add and delete

--- 06-analytics-engineering-greenery-project/scripts/init.sql
+++ 06-analytics-engineering-greenery-project/scripts/init.sql
@@ -1,20 +1,73 @@
-CREATE TABLE IF NOT EXISTS address (
-    address_id TEXT PRIMARY KEY,
-    address TEXT,
-    zipcode INT,
-    state TEXT,
-    country TEXT
+CREATE TABLE addresses (
+  address_id VARCHAR(256) PRIMARY KEY,
+  address VARCHAR(8192),
+  zipcode INTEGER,
+  state VARCHAR(256),
+  country VARCHAR(256)
 );
 
-COPY address FROM '06-analytics-engineering/greenery/data/data/addresses.csv' DELIMITER ',' CSV HEADER;
+CREATE TABLE users (
+  user_id VARCHAR(256) PRIMARY KEY,
+  first_name VARCHAR(256),
+  last_name VARCHAR(256),
+  email VARCHAR(1024),
+  phone_number VARCHAR(256),
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  address_id VARCHAR(256) REFERENCES addresses(address_id)
+);
 
--- CREATE TABLE IF NOT EXISTS events (
---     event_id TEXT PRIMARY KEY,
---     session_id TEXT,
---     user_id INT,
---     page_url TEXT,
---     created_at TEXT,
---     event_type TEXT,
---     order_id TEXT,
---     product_id TEXT
--- );
+CREATE TABLE promos (
+  promo_id VARCHAR(256) PRIMARY KEY,
+  discount INTEGER,
+  status VARCHAR(128)
+);
+
+CREATE TABLE products (
+  product_id VARCHAR(256) PRIMARY KEY,
+  name VARCHAR(1024),
+  price REAL,
+  inventory INTEGER
+);
+
+CREATE TABLE orders (
+  order_id VARCHAR(256) PRIMARY KEY,
+  user_id VARCHAR(256) REFERENCES users(user_id),
+  promo_id VARCHAR(256) REFERENCES promos(promo_id),
+  address_id VARCHAR(256) REFERENCES addresses(address_id),
+  created_at TIMESTAMP,
+  order_cost REAL,
+  shipping_cost REAL,
+  order_total REAL,
+  tracking_id VARCHAR(256),
+  shipping_service VARCHAR(128),
+  estimated_delivery_at TIMESTAMP,
+  delivered_at TIMESTAMP,
+  status VARCHAR(128)
+);
+
+CREATE TABLE order_items (
+  order_id VARCHAR(256) REFERENCES orders(order_id),
+  product_id VARCHAR(256) REFERENCES products(product_id),
+  quantity INTEGER,
+  PRIMARY KEY(order_id, product_id)
+);
+
+CREATE TABLE events(
+  event_id VARCHAR(256) PRIMARY KEY,
+  session_id VARCHAR(256),
+  user_id VARCHAR(256) REFERENCES users(user_id),
+  page_url VARCHAR(4096),
+  created_at TIMESTAMP,
+  event_type VARCHAR(128),
+  order_id VARCHAR(256) REFERENCES orders(order_id),
+  product_id VARCHAR(256) REFERENCES products(product_id)
+);
+
+COPY addresses FROM '06-analytics-engineering-greenery-project/data/addresses.csv' DELIMITER ',' CSV HEADER;
+COPY users FROM '06-analytics-engineering-greenery-project/data/users.csv' DELIMITER ',' CSV HEADER;
+COPY promos FROM '06-analytics-engineering-greenery-project/data/promos.csv' DELIMITER ',' CSV HEADER;
+COPY products FROM '06-analytics-engineering-greenery-project/data/products.csv' DELIMITER ',' CSV HEADER;
+COPY orders FROM '06-analytics-engineering-greenery-project/data/orders.csv' DELIMITER ',' CSV HEADER;
+COPY order_items FROM '06-analytics-engineering-greenery-project/data/order_items.csv' DELIMITER ',' CSV HEADER;
+COPY events FROM '06-analytics-engineering-greenery-project/data/events.csv' DELIMITER ',' CSV HEADER;
